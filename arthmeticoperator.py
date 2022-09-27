ae9daf434af57d73fe6b33c3302dd8f3d027d577
ishigangwar <ig2987@srmist.edu.in>
2022-09-27 14:39:06 +0530

    ADDED CODES

---
+++ arthmeticoperator.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+    add=a+b
+    difference=a-b
+    multi=a*b
+    print(add)
+    print(difference)
+    print(multi)
---
+++ printfunc.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(n):
+        print(i+1,end="")
